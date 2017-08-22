0f4e780f1fe5150f642f6e13d033ca1d34577459
Ikemma Ifunanya <ifavour902@gmail.com>
2017-08-22 20:08:16 +0100

    made some corrections

--- media.py
+++ media.py
@@ -1,16 +1,17 @@
 import webbrowser
 
-#class definition.
+# class definition.
+
+
 class favourite_movies():
+        # constructor for the class favourite_movies.
+        def __init__(self, favourite_movie_title, favourite_movie_image, favourite_movie_url):  # NOQA
+            # initializes favourite movies attributes
+            self.title = favourite_movie_title
+            self.collection_image = favourite_movie_image
+            self.collection_youtube_url = favourite_movie_url
 
-	#constructor for the class favourite_movies.
-    def __init__(self, favourite_movie_title,favourite_movie_image, favourite_movie_url):
-    	#initializes favourite movies attributes
-        self.title = favourite_movie_title
-        self.collection_image = favourite_movie_image
-        self.collection_youtube_url = favourite_movie_url
 
-
-    def display_favourite_movies(self):
-    	#opens the collection_youtube_url  in the default browser.
-        webbrowser.open(self.collection_youtube_url)
+def display_favourite_movies(self):
+    # opens the collection_youtube_url  in the default browser.
+    webbrowser.open(self.collection_youtube_url)
